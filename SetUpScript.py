e9f4b6be8258415ddfa9b6991171134552ed87f7
otowmoyarng <hidemusiam@gmail.com>
2019-08-04 22:34:14 +0900

    qiitaでのフィードバックを反映した

--- SetUpScript.py
+++ SetUpScript.py
@@ -1,16 +1,22 @@
 """
 セットアップ用スクリプト
-Setting.iniファイルの初期設定を行う。
+設定ファイルの初期設定を行う。
 """
 from Settings import Settings
 
-config = Settings()
+print("設定ファイルの初期設定を開始します。")
+print()
+
+print("設定ファイル名を入力してください。省略するとsetting.iniとなります。")
+filename = input("設定ファイル名：")
+if filename is None or filename == '':
+    filename = 'setting.ini'
+    config = Settings()
+else:
+    config = Settings(filename)
 cpins = config.getConfigParserInstance()
 
-print("Setting.iniの初期設定を開始します。")
-print()
 print("LINE Notify登録時のアクセストークンを入力してください。")
-
 token = input('アクセストークン：')
 cpins.set(config.NOTIFY, config.NOTIFY_TOKEN, token)
 print("アクセストークンを登録しました。")
@@ -18,8 +24,8 @@ print("アクセストークンを登録しました。")
 cpins.set(config.NOTIFY, config.NOTIFY_APIURL, 'https://notify-api.line.me/api/notify')
 print("API用URLを登録しました。")
 
-with open('setting.ini', 'w') as contextfile:
+with open(filename, 'w') as contextfile:
     cpins.write(contextfile)
 
 print()
-print("Setting.iniの初期設定が完了しました。")
+print("設定ファイルの初期設定が完了しました。")
